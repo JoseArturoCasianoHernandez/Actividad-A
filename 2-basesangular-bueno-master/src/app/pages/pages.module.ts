56e7edbd9c326fe52b5a5f7d04d4b1480f770667
Jose Arturo <josseartturo150203@gmail.com>
2022-07-15 19:10:36 -0500

    Initial commit

---
+++ 2-basesangular-bueno-master/src/app/pages/pages.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
+// import { CommentsPipe } from './comments.pipe';
+
+
+
+@NgModule({
+  declarations: [
+    AboutComponent,
+    ContactComponent,
+    HomeComponent,
+    // CommentsPipe,
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    AboutComponent,
+    ContactComponent,
+    HomeComponent,
+  ]
+})
+export class PagesModule { }
